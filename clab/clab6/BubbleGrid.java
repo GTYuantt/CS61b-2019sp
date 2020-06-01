fcc3720932aa7e96bde06a970d5f129381ef9661
GTYuantt <zju_gty0312@163.com>
2020-06-02 01:06:42 +0800

    clab6 is completed

--- clab/clab6/BubbleGrid.java
+++ clab/clab6/BubbleGrid.java
@@ -1,11 +1,18 @@
 public class BubbleGrid {
     private int[][] grid;
+    private int rowNum;
+    private int columnNum;
+    int ceiling;
+
 
     /* Create new BubbleGrid with bubble/space locations specified by grid.
      * Grid is composed of only 1's and 0's, where 1's denote a bubble, and
      * 0's denote a space. */
     public BubbleGrid(int[][] grid) {
         this.grid = grid;
+        this.rowNum = grid.length;
+        this.columnNum = grid[0].length;
+        this.ceiling = rowNum*columnNum;
     }
 
     /* Returns an array whose i-th element is the number of bubbles that
@@ -14,6 +21,91 @@ public class BubbleGrid {
      * and have no side-effects to grid. */
     public int[] popBubbles(int[][] darts) {
         // TODO
-        return null;
+        int[] bubblesFall = new int[darts.length];
+        for(int i = 0; i < darts.length;i++){
+            bubblesFall[i] = popBubbles(darts[i]);
+        }
+        return bubblesFall;
     }
+
+    public int popBubbles(int[] dart){
+
+        int rowOfDart = dart[0];
+        int columnOfDart = dart[1];
+        UnionFind beforeUnion = numberOfStuckBubbles(grid);
+        int beforeNumber = beforeUnion.sizeOf(ceiling)-1;
+        grid[rowOfDart][columnOfDart] = 0;
+        UnionFind afterUnion = numberOfStuckBubbles(grid);
+        int afterNumber = afterUnion.sizeOf(ceiling)-1;
+        clearFallBubbles(afterUnion);
+        if(beforeNumber==afterNumber){
+            return 0;
+        }
+        else {
+            return beforeNumber-afterNumber-1;
+        }
+    }
+
+    public void clearFallBubbles(UnionFind bg){
+        for(int row=0;row<rowNum;row++){
+            for (int col=0;col<columnNum;col++){
+                if(grid[row][col]==1){
+                    if(!bg.connected(xyTo1D(row,col),ceiling)){
+                        grid[row][col]=0;
+                    };
+                }
+            }
+        }
+    }
+
+    public UnionFind numberOfStuckBubbles(int[][] grid){
+        UnionFind bg = new UnionFind(rowNum*columnNum+1); //the last element is the ceiling;
+
+
+        for (int i=0;i<columnNum;i++){
+            if(grid[0][i]==1){
+                bg.union(xyTo1D(0,i),ceiling);
+            }
+        }
+        for (int row=1;row<rowNum;row++){
+            for(int col=0;col<columnNum;col++){
+                if(grid[row][col]==1){
+                    unionNeighbors(row,col,bg);
+                }
+            }
+        }
+
+        return bg;
+    }
+
+    public void unionNeighbors(int row, int col, UnionFind bg){
+        int adjrow = row+1;
+        int adjcol = col;
+        if(adjrow < rowNum && grid[adjrow][adjcol]==1) {
+            bg.union(xyTo1D(row, col), xyTo1D(adjrow, adjcol));
+        }
+        adjrow = row-1;
+        adjcol = col;
+        if(adjrow >= 0 && grid[adjrow][adjcol]==1) {
+            bg.union(xyTo1D(row, col), xyTo1D(adjrow, adjcol));
+        }
+        adjrow = row;
+        adjcol = col+1;
+        if(adjcol < columnNum && grid[adjrow][adjcol]==1) {
+            bg.union(xyTo1D(row, col), xyTo1D(adjrow, adjcol));
+        }
+        adjrow = row;
+        adjcol = col-1;
+        if(adjcol >= 0 && grid[adjrow][adjcol]==1) {
+            bg.union(xyTo1D(row, col), xyTo1D(adjrow, adjcol));
+        }
+    }
+
+
+    public int xyTo1D (int row, int column){
+        return column + row*columnNum;
+    }
+
+
+
 }
--- clab/clab6/UnionFind.java
+++ clab/clab6/UnionFind.java
@@ -1,51 +1,96 @@
 public class UnionFind {
 
     // TODO - Add instance variables?
+    private int[] parent;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
         // TODO
+        parent = new int[n];
+        for (int i = 0;i < parent.length;i++){
+            parent[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
         // TODO
+        if (vertex < 0 || vertex >= parent.length){
+            throw new IllegalArgumentException("Invalid index");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
         // TODO
-        return -1;
+        validate(v1);
+        int root = find(v1);
+        int size = Math.abs(parent(root));
+        return size;
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
         // TODO
-        return -1;
+        validate(v1);
+        return parent[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
         // TODO
-        return false;
+        validate(v1);
+        validate(v2);
+        int rootOfV1 = find(v1);
+        int rootOfV2 = find(v2);
+        return rootOfV1 == rootOfV2;
     }
 
-    /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
+    /* Connects two elements v1 and v2 together. v1 and v2 can be any valid
        elements, and a union-by-size heuristic is used. If the sizes of the sets
-       are equal, tie break by connecting v1's root to v2's root. Unioning a 
-       vertex with itself or vertices that are already connected should not 
+       are equal, tie break by connecting v1's root to v2's root. Unioning a
+       vertex with itself or vertices that are already connected should not
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
         // TODO
+        validate(v1);
+        validate(v2);
+        int rootOfV1 = find(v1);
+        int rootOfV2 = find(v2);
+        int sizeV1 = sizeOf(v1);
+        int sizeV2 = sizeOf(v2);
+        if (v1 == v2 || rootOfV1 == rootOfV2){
+            return;
+        }
+        if (sizeV1 <= sizeV2){
+            parent[rootOfV1] = rootOfV2;
+            parent[rootOfV2] = -(sizeV1 + sizeV2);
+        }
+        else {
+            parent[rootOfV2] = rootOfV1;
+            parent[rootOfV1] = -(sizeV1 + sizeV2);
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
         // TODO
-        return -1;
+        validate(vertex);
+        int[] path = new int[parent.length];
+        int sizeOfPath = 0;
+        while (parent(vertex) >= 0){
+            path[sizeOfPath] = vertex;
+            sizeOfPath++;
+            vertex = parent(vertex);
+        }
+        for (int i = 0; i < sizeOfPath; i++){
+            parent[path[i]] = vertex;
+        }
+
+        return vertex;
     }
 
 }
